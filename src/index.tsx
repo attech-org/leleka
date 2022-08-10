7f07fafb3ac4664ba1e6a71ba5bd9703ee373267
alexandrtovmach <alexandrtovmach@gmail.com>
2022-08-10 19:23:18 +0200

    feat: Added reset.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import "./styles/reset.css";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
