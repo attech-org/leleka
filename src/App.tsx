ba4059e9c928808bf86c8ab6dd03d6ff587433dd
alexandrtovmach <alexandrtovmach@gmail.com>
2022-08-10 18:31:06 +0200

    feat: Added styling tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  all: unset;
+  padding: 0.5rem 1rem;
+  background-color: violet;
+  border-radius: 5px;
+  text-transform: uppercase;
+`
 
 const App: React.FunctionComponent = () => (
   <div>
     Leleka is running...
+    <Button>hit me!</Button>
   </div>
 );
 
