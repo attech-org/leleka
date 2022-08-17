ffaa2085d64b311581886b8ed69a9f88a829f9b2
DavidKorystov <david.korystov@gmail.com>
2022-08-17 18:05:59 +0300

    fix: Fixed bugs

--- src/containers/LoginForm.tsx
+++ src/containers/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import {
   Container,
   Form,
@@ -8,44 +9,48 @@ import {
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-// font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
-
-const OnHoverClose = styled.div`
-  :hover {
-    background: rgb(15, 20, 25, 0.1);
-    transition-duration: 0.2s;
-  }
-`;
+import * as yup from "yup";
 
 interface MyForm {
   userName: string;
-  email: string;
-  dateOfBirth: Date;
-  password: string;
-  confirmPassword: string;
 }
 
-export const LoginForm = () => {
-  const { register, handleSubmit } = useForm<MyForm>();
+const sampleSchema = yup.object().shape({
+  userName: yup.string().required("Name is required!"),
+});
+
+const StyledContainer = styled(Container)`
+  height: 650px;
+  width: 600px;
+`;
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<MyForm>({
+    resolver: yupResolver(sampleSchema),
+  });
 
   const submitForm = (data: MyForm) => {
-    return data;
-    // console.log(data);
+    console.log(data);
+    reset();
   };
 
   return (
-    <Container
-      style={{ height: 650, width: 600 }}
-      className=" rounded-4  p-2   "
-    >
+    <StyledContainer className=" rounded-4 p-2">
       <header className="d-flex justify-content-between align-items-center">
         <OverlayTrigger
           key={"bottom"}
           placement={"bottom"}
-          overlay={<Tooltip id={`tooltip-${"bottom"}`}>Close</Tooltip>}
+          overlay={<Tooltip id={`tooltip-bottom}`}>Close</Tooltip>}
         >
-          <OnHoverClose className="p-2 d-flex justify-content-center align-items-center rounded-5">
+          <Button
+            variant="light"
+            className="d-block p-2 d-flex justify-content-center align-items-center rounded-5"
+          >
             <svg
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -56,7 +61,7 @@ export const LoginForm = () => {
                 <path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z" />
               </g>
             </svg>
-          </OnHoverClose>
+          </Button>
         </OverlayTrigger>
 
         <svg
@@ -69,16 +74,16 @@ export const LoginForm = () => {
             <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z" />
           </g>
         </svg>
-        <div style={{ height: 32, width: 20 }} />
+        <div className="p-3" />
       </header>
 
-      <section style={{ width: 300 }} className="mt-4 m-auto  d-grid gap-4">
+      <section className="w-50 mt-4 m-auto d-grid gap-4">
         <h1 className="fs-1 fw-700 ">Sign in to Twitter</h1>
 
         <Form onSubmit={handleSubmit(submitForm)} className="d-grid gap-2 ">
           <Button
             variant="light"
-            className="d-flex justify-content-center align-items-center fw-500 w-100 d-block mb-3 rounded-5 py-2 border  border-gray-400"
+            className="d-flex justify-content-center align-items-center fw-500 w-100 d-block mb-3 rounded-5 py-2 border border-gray-400"
           >
             <svg
               className="me-2"
@@ -134,25 +139,32 @@ export const LoginForm = () => {
           </Button>
 
           <div className="d-flex justify-content-between align-items-center">
-            <div className="bg-secondary border-bottom border-secondary w-50 " />
+            <div className="bg-secondary border-bottom border-secondary w-50" />
             <div className="fs-5 text-secondary mx-2">or</div>
             <div className="bg-secondary border-bottom border-secondary w-50" />
           </div>
 
           <div className="form-floating mb-3">
             <input
-              {...register("email")}
-              type="email"
-              name="email"
-              className="form-control "
+              {...register("userName")}
+              type="text"
+              name="userName"
+              className="form-control"
               id="floatingInput"
-              placeholder="name@example.com"
+              placeholder="Name"
             />
             <label>Phone, email, or username</label>
           </div>
+          {errors.userName && (
+            <div className="position-absolute start-0 end-0 bottom-0 text-center mb-5 text-white">
+              <span className="bg-primary rounded-1 px-4 p-3">
+                {errors?.userName?.message}
+              </span>
+            </div>
+          )}
 
           <Button
-            className="btn btn-dark fw-700 w-100 d-block mb-3 rounded-5 py-2  border border-0"
+            className="btn btn-dark fw-700 w-100 d-block mb-3 rounded-5 py-2 border border-0"
             type="submit"
           >
             Next
@@ -160,20 +172,20 @@ export const LoginForm = () => {
 
           <Button
             variant="light"
-            className=" fw-700 w-100 d-block mb-4 rounded-5 py-2 border  border-gray-400"
+            className="fw-700 w-100 d-block mb-4 rounded-5 py-2 border border-gray-400"
           >
             Forgot password?
           </Button>
           <div>
-            <span style={{ color: "rgb(83, 100, 113)" }} className="">
-              Don't have an account?
-            </span>
-            <Link className="ms-1 text-decoration-none" to="/signup">
+            <span className="text-secondary">Don't have an account?</span>
+            <Link className="ms-1 text-decoration-none" to="/SignUpModal">
               Sign up
             </Link>
           </div>
         </Form>
       </section>
-    </Container>
+    </StyledContainer>
   );
 };
+
+export default LoginForm;
