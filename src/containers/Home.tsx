2175feba6c881934d9a3cd7b31eb055da32144a9
Barylo <dmitrobarilo@gmail.com>
2022-08-18 14:19:00 +0300

    deleted comments

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,15 +1,7 @@
-import { InputLine } from "../pages/InputLine";
-import { CommentsList } from "./CommentsList";
-import { TweetPage } from "./TweetPage";
-
 const HomeContainer: React.FunctionComponent = () => {
   return (
     <div>
       <div>Home container</div>
-
-      <TweetPage />
-      <InputLine />
-      <CommentsList />
     </div>
   );
 };
