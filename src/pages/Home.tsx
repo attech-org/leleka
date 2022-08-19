d736a18e622733e1c27fe69bac7b1db613a8af55
Timur Bolotskij <timurbolotskij@gmail.com>
2022-08-19 17:32:58 +0300

    Added Tabs Container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import FormValidationSample from "../components/FormValidationSample";
 import HomeContainer from "../containers/Home";
 import Layout from "../containers/Layout";
-import TabsContainer from "../containers/Tabs";
 const HomePage: React.FunctionComponent = () => {
   return (
     <Layout>
       <HomeContainer />
       <FormValidationSample />
-      <TabsContainer />
     </Layout>
   );
 };
