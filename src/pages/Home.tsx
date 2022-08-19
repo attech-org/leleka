19158b6fe57efb40a705d701d0ce9aee56e6f52c
KirillSerg <kirill.seregienko1989@gmail.com>
2022-08-19 21:40:26 +0300

    fix: change svg to bootstrap icons in login form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HomeContainer from "../containers/Home";
 import Layout from "../containers/Layout";
+import LoginForm from "../containers/LoginForm";
 
 const HomePage: React.FunctionComponent = () => {
   return (
     <>
       <Layout>
         <HomeContainer />
+        <LoginForm />
       </Layout>
     </>
   );
