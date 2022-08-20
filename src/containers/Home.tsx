1753e6536cffe649295ea032892fc61ed24978fe
Barylo <dmitrobarilo@gmail.com>
2022-08-20 14:04:34 +0300

    fix: debuged

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,11 +1,14 @@
 import { useTranslation } from "react-i18next";
 
+import { TweetPage } from "./TweetPage";
+
 const HomeContainer: React.FunctionComponent = () => {
   const { t } = useTranslation();
   return (
     <>
       <div>Home container</div>
       <p>{t("test")}</p>
+      <TweetPage />
     </>
   );
 };
