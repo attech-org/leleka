aee9e0a1068b8636e19ad4e5d601ff99f44d9f24
Artem Ligerko <artmarket81@gmail.com>
2022-08-21 14:31:55 +0300

    feat: created bookmarks container

--- src/containers/BookmarksList.tsx
+++ src/containers/BookmarksList.tsx
@@ -1,23 +1,66 @@
 import React from "react";
-import { Navbar, Row, Col, Nav } from "react-bootstrap";
+import { Button, Navbar, OverlayTrigger, Popover } from "react-bootstrap";
+import { ThreeDots } from "react-bootstrap-icons";
+import styled from "styled-components";
 
 import Bookmark from "../components/SingleTweetComment";
 import { bookmarksData } from "../MOCKS/bookmarks";
 
+const StyledNavbar = styled(Navbar)`
+  background-color: rgba(255, 255, 255, 0.97) !important;
+`;
+
+const StyledDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledPopover = styled(Popover)`
+  .popover-arrow {
+    display: none;
+  }
+`;
+
+const StyledThreeDots = styled(ThreeDots)`
+  height: 32px;
+  width: 32px;
+  &:hover {
+    background-color: #c5c5c5;
+    transition: 0.5s;
+  }
+`;
+
 const BookmarksList: React.FC = () => {
   return (
     <div>
-      <Navbar sticky="top" expand="false" variant="light" bg="white">
-        <Row xs="auto" className=" justify-content-between">
-          <Col className="mx-2">
-            <Nav.Link>Bookmarks</Nav.Link>
-            <Nav.Link>@artlee4</Nav.Link>
-          </Col>
-          <Col className="mx-2">
-            <Nav.Link>dots</Nav.Link>
-          </Col>
-        </Row>
-      </Navbar>
+      <StyledNavbar sticky="top" expand="false" variant="light" bg="white">
+        <StyledDiv>
+          <div>
+            <Navbar.Brand className="fw-bold px-3 py-1 m-0 row justify-comtent-start">
+              Bookmarks
+            </Navbar.Brand>
+            <div className="px-3 py-0 m-0 row justify-comtent-start">
+              @artlee4
+            </div>
+          </div>
+          <div>
+            <OverlayTrigger
+              rootClose
+              trigger="click"
+              key="left"
+              placement="left"
+              overlay={
+                <StyledPopover>
+                  <Button variant="light"> Видалити всі закладки</Button>
+                </StyledPopover>
+              }
+            >
+              <StyledThreeDots className="m-2 p-2 rounded-circle" />
+            </OverlayTrigger>
+          </div>
+        </StyledDiv>
+      </StyledNavbar>
       {bookmarksData.map((item) => (
         <Bookmark
           key={item.id}
