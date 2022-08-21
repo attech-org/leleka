1c626850a077b5203e97562e4590be2573d36f94
Barylo <dmitrobarilo@gmail.com>
2022-08-21 16:49:58 +0300

    fix: repaired the view of props

--- src/containers/CommentsList.tsx
+++ src/containers/CommentsList.tsx
@@ -6,19 +6,8 @@ import { tweetCommentsData } from "../MOCKS/singleTweetPage";
 export const CommentsList: React.FC = () => {
   return (
     <div>
-      {tweetCommentsData.map((obj) => (
-        <SingleTweetComment
-          key={obj.id}
-          userlogo={obj.userlogo}
-          username={obj.username}
-          userNickname={obj.userNickname}
-          responserUserNickname={obj.responserUserNickname}
-          messegeText={obj.messegeText}
-          messegeDate={obj.messegeDate}
-          answerCount={obj.answerCount}
-          retweetCount={obj.retweetCount}
-          likeCount={obj.likeCount}
-        />
+      {tweetCommentsData.map((comment) => (
+        <SingleTweetComment key={comment.id} {...comment} />
       ))}
     </div>
   );
