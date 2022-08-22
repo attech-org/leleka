996cb7bccc7db31babf56cd9b5ebe3e947c32665
Artem Ligerko <artmarket81@gmail.com>
2022-08-22 21:04:24 +0300

    feat: creating tweet creation form

--- src/components/TweetCreationForm.tsx
+++ src/components/TweetCreationForm.tsx
@@ -1,6 +1,5 @@
-// import React from "react";
-import { Button, Form, Stack } from "react-bootstrap";
-import { EmojiSmile, Image, Globe2 } from "react-bootstrap-icons";
+import { Button, Form, Row, Col, Container } from "react-bootstrap";
+import { EmojiSmile, Image as ImageIcon, Globe2 } from "react-bootstrap-icons";
 import styled from "styled-components";
 
 const StyledFormControl = styled(Form.Control)`
@@ -8,59 +7,71 @@ const StyledFormControl = styled(Form.Control)`
     box-shadow: none;
   }
 `;
+const StyledButton = styled(Button)`
+  &:hover {
+    box-shadow: none;
+  }
+`;
+const StyledIcon = styled.div`
+  height: 32px;
+  width: 32px;
+  color: #2929ff;
+`;
 
 const TweetCreationForm = () => {
   return (
     <div>
-      <div className="justify-content-start">
-        <div>
-          <img
-            src="http://dummyimage.com/105x100.png/5fa2dd/ffffff"
-            width="48px"
-            height="48px"
-            className="me-auto rounded-circle"
-          />
-        </div>
-        <div>
-          container
-          <div>
-            <StyledFormControl
-              placeholder="Що відбувається?"
-              className="border-0"
-            />
-          </div>
-          <div>
-            <Button>
-              <Globe2 />
-              Відповідати можусть усі користувачі
-            </Button>
-          </div>
-          <div>
-            <Image />
-            <EmojiSmile />
-          </div>
-        </div>
-      </div>
-
-      <Form>
-        <Form.Group className="justify-comtent-start">
-          <Stack direction="horizontal" gap={2} className="mx-2">
+      <Container className="p-2">
+        <Row>
+          <Col xs={{ span: 1, offset: 0 }}>
             <img
               src="http://dummyimage.com/105x100.png/5fa2dd/ffffff"
               width="48px"
               height="48px"
               className="me-auto rounded-circle"
             />
+          </Col>
+          <Col xs={{ span: 11, offset: 0 }}>
             <StyledFormControl
               placeholder="Що відбувається?"
               className="border-0"
+              size="lg"
             />
-          </Stack>
-        </Form.Group>
-        <Form.Group>
-          <Button>Відповідати можусть усі користувачі</Button>
-        </Form.Group>
-      </Form>
+
+            <Container className="border-bottom">
+              <StyledButton
+                size="sm"
+                className="my-2 d-flex align-items-center secondary rounded-5 fw-bold text-primary bg-white border-0"
+              >
+                <Globe2 className="me-1" />
+                <span>Відповідати можусть усі користувачі</span>
+              </StyledButton>
+            </Container>
+
+            <Container>
+              <Row size="lg">
+                <Col
+                  xs={{ span: 1, offset: 0 }}
+                  className="d-flex justify-content-start align-items-center p-2"
+                >
+                  <StyledIcon>
+                    <ImageIcon className="mx-1" />
+                  </StyledIcon>
+                  <StyledIcon>
+                    <EmojiSmile className="mx-1" />
+                  </StyledIcon>
+                </Col>
+                <Col
+                  xs={{ span: 11, offset: 0 }}
+                  className="d-flex justify-content-end align-items-center p-2"
+                >
+                  <Button className="rounded-5">Твітнути</Button>
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
