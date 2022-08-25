4133fd88ab56c237e8a2a153d0abb53dbd4682a3
DavidKorystov <david.korystov@gmail.com>
2022-08-25 21:42:24 +0300

    feat: moved logic to services & replaced classes

--- src/components/LinkPreview.tsx
+++ src/components/LinkPreview.tsx
@@ -1,64 +1,37 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { Card, Button } from "react-bootstrap";
+
+import { getLinkPreview } from "../services/linkPreview";
 
 interface PropsUrl {
   url: string;
 }
 
-const Wrapper = styled.div`
-  width: 300px;
-`;
-
-const LinkPreview: React.FC<PropsUrl> = ({ url }) => {
-  const dataObject = {
-    key: "5077f2d1873bbe183521eb490ff0e2bb",
-    q: url,
-  };
-
-  const [responseObject, setResponseobject] = useState({
+const LinkPreview = ({ url }: PropsUrl) => {
+  const MOCK = {
     description: "",
     image: "",
     title: "",
-    url: "",
-  });
+    url,
+  };
+
+  const [response, setResponse] = useState(MOCK);
 
   useEffect(() => {
-    fetch("https://api.linkpreview.net", {
-      method: "POST",
-      mode: "cors",
-      body: JSON.stringify(dataObject),
-    })
-      .then((res) => {
-        if (res.status != 200) {
-          // console.log(res.status);
-          throw new Error("something went wrong");
-        }
-        return res.json();
-      })
-      .then((response) => {
-        setResponseobject(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    getLinkPreview(url).then((res) => setResponse(res));
   }, []);
 
   return (
-    <Wrapper className="card">
-      <img src={responseObject.image} className="card-img-top" alt="preview" />
-
-      <div className="card-body ">
-        <h5 className="card-title  fw-bold">{responseObject.title}</h5>
-        <p className="card-text">{responseObject.description}</p>
-        <a
-          href={responseObject.url}
-          target="blank"
-          className="btn btn-light d-block mt-3"
-        >
-          {responseObject.url}
-        </a>
-      </div>
-    </Wrapper>
+    <Card className="d-flex flex-column align-items-center">
+      <Card.Img variant="top" src={response.image} />
+      <Card.Body>
+        <Card.Title>{response.title}</Card.Title>
+        <Card.Text>{response.description}</Card.Text>
+        <Button href={response.url} target="blank" variant="light">
+          {response.url}
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
