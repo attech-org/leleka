ce830d982f43c321a47fc1eb87663bdf7574f877
DavidKorystov <david.korystov@gmail.com>
2022-08-26 20:47:13 +0300

    feat/replaced services/preview on customFether

--- src/components/LinkPreview.tsx
+++ src/components/LinkPreview.tsx
@@ -1,34 +1,56 @@
 import { useEffect, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 
-import { getLinkPreview } from "../services/linkPreview";
+import { customFetcher } from "../services/customFetcher";
 
+export interface APIOutput {
+  title?: string;
+  description?: string;
+  image?: string;
+  siteName?: string;
+  hostname?: string;
+  url?: string;
+}
 interface PropsUrl {
   url: string;
 }
 
 const LinkPreview = ({ url }: PropsUrl) => {
-  const MOCK = {
+  const MOCK: APIOutput = {
     description: "",
     image: "",
     title: "",
+    siteName: "",
+    hostname: "",
     url,
   };
 
   const [response, setResponse] = useState(MOCK);
 
   useEffect(() => {
-    getLinkPreview(url).then((res) => setResponse(res));
+    customFetcher(url).then((res) => {
+      if (res.image[0] !== "h") {
+        res.image = "https://" + res.hostname + res.image;
+      }
+      setResponse(res);
+    });
   }, []);
 
   return (
-    <Card className="d-flex flex-column align-items-center">
-      <Card.Img variant="top" src={response.image} />
+    <Card className="text-start">
+      <Card.Img src={response.image} />
       <Card.Body>
         <Card.Title>{response.title}</Card.Title>
         <Card.Text>{response.description}</Card.Text>
-        <Button href={response.url} target="blank" variant="light">
-          {response.url}
+        <Button
+          className="mt-3 d-flex align-items-center"
+          href={response.url}
+          target="blank"
+          variant="light"
+        >
+          {response.siteName}
+          <span className="mb-1 mx-1">.</span>
+          {response.hostname}
         </Button>
       </Card.Body>
     </Card>
---
+++ src/services/customFetcher.ts
@@ -0,0 +1,8 @@
+export const customFetcher = async (url: string) => {
+  const response = await fetch(
+    `http://127.0.0.1:3001/api/proxyLinkPreview/?url=${url}`
+  );
+  const json = await response.json();
+  // console.warn(json.metadata);
+  return json.metadata;
+};
