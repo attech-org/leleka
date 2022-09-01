bd4295b506085f01b89d756a9e94b832850ee93f
alexandrtovmach <alexandrtovmach@gmail.com>
2022-09-01 18:39:00 +0200

    fix: Resolved issue with types

--- src/containers/TweetsWithReplies.tsx
+++ src/containers/TweetsWithReplies.tsx
@@ -2,10 +2,17 @@ import SingleTweet from "../components/SingleTweet";
 import tweets from "../MOCKS/tweets";
 import InfiniteList from "./InfiniteList";
 
+const mockPagination = {
+  docs: tweets,
+  page: 1,
+  limit: 10,
+  hasNextPage: false,
+};
+
 export const TweetsWithReplies: React.FunctionComponent = () => {
   return (
     <InfiniteList
-      data={tweets}
+      data={mockPagination}
       showMore={() => {}}
       itemComponent={(itemData) => <SingleTweet {...itemData} />}
     />
