7ac448850db755d92a6ab1ca4bc5a9ba45628b29
Mykyta Pysarev <mykyta.pisarev@gmail.com>
2022-09-06 01:27:57 +0300

    feat: added toast component to handle errors in reducers

---
+++ src/containers/ErrorsToaster.tsx
@@ -0,0 +1,34 @@
+// import { useState } from "react";
+import { Toast, ToastContainer } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../redux/reducers";
+
+const ErrorsToaster = () => {
+  const tweetsFetch = useSelector<RootState, RootState["tweets"]["feedTweets"]>(
+    (store) => store.tweets.feedTweets
+  );
+  return (
+    <>
+      <ToastContainer position="top-center">
+        {tweetsFetch.isLoading && tweetsFetch.error ? (
+          <Toast bg="danger">
+            <Toast.Body>{`${tweetsFetch.error}`}</Toast.Body>
+          </Toast>
+        ) : null}
+        {tweetsFetch.isLoading && tweetsFetch.error ? (
+          <Toast bg="danger">
+            <Toast.Body>{`Another ERROR`}</Toast.Body>
+          </Toast>
+        ) : null}
+        {tweetsFetch.isLoading && tweetsFetch.error ? (
+          <Toast bg="danger">
+            <Toast.Body>{`And another ERROR`}</Toast.Body>
+          </Toast>
+        ) : null}
+      </ToastContainer>
+    </>
+  );
+};
+
+export default ErrorsToaster;
