53cba6261a721975d7e6b392d83226bf1e9fa9ce
DmytroTikhostup <nmworkfed@gmail.com>
2022-09-11 13:04:44 +0300

    feat/fix left and right panels

--- src/components/RightPanel.tsx
+++ src/components/RightPanel.tsx
@@ -5,10 +5,18 @@ import Recommendations from "../containers/Recommendations";
 import Trends from "../containers/Trends";
 
 const Wrapper = styled.div``;
+
+const StickySearch = styled.div`
+  position: sticky;
+  top: 1.5%;
+`;
+
 export const RightPanel = () => {
   return (
     <Wrapper className="col-xl-4 col-lg-4">
-      <AsyncSearch />
+      <StickySearch>
+        <AsyncSearch />
+      </StickySearch>
       {!window.location.pathname.includes("/trends") && <Trends />}
       <Recommendations />
     </Wrapper>
