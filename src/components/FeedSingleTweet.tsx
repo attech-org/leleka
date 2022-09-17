99f6ebeb458ac498bb20f67b78e7523c47c84085
Nik <103769012+Mykyta-Pysarev@users.noreply.github.com>
2022-09-17 23:47:05 +0300

    Update src/components/FeedSingleTweet.tsx
    Co-authored-by: Alexandr Tovmach <alexandrtovmach@gmail.com>

--- src/components/FeedSingleTweet.tsx
+++ src/components/FeedSingleTweet.tsx
@@ -95,7 +95,7 @@ const FeedSingleTweet = ({
     "/https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}/"
   );
 
-  const url = content.match(urlSearch) || "";
+  const url = content.match(urlSearch) || [];
 
   return (
     <>
