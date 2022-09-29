426c22f2fbfb95050369b16c9fd7c752d945bdc4
Barylo <dmitrobarilo@gmail.com>
2022-09-29 20:50:58 +0300

    deleted all mocks

--- src/containers/CommentsList.tsx
+++ src/containers/CommentsList.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-import SingleTweetComment from "../components/SingleTweetComment";
-import { tweetCommentsData } from "../MOCKS/singleTweetPage";
+// import SingleTweetComment from "../components/SingleTweetComment";
+// import { tweetCommentsData } from "../MOCKS/singleTweetPage";
 
 export const CommentsList: React.FC = () => {
   return (
     <div>
-      {tweetCommentsData.map((comment) => (
+      {/* {tweetCommentsData.map((comment) => (
         <SingleTweetComment key={comment.id} {...comment} />
-      ))}
+      ))} */}
     </div>
   );
 };
