25f81ed0f91064809bc54467dfe4029c16c1c083
Barylo <dmitrobarilo@gmail.com>
2022-09-29 21:09:10 +0300

    deleted comments from commentsList

--- src/containers/CommentsList.tsx
+++ src/containers/CommentsList.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 
-// import SingleTweetComment from "../components/SingleTweetComment";
-// import { tweetCommentsData } from "../MOCKS/singleTweetPage";
-
 export const CommentsList: React.FC = () => {
-  return (
-    <div>
-      {/* {tweetCommentsData.map((comment) => (
-        <SingleTweetComment key={comment.id} {...comment} />
-      ))} */}
-    </div>
-  );
+  return <div />;
 };
